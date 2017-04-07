26f73a749deca374207a4e8b9531ebbbb7ffd756
xiong116276 <1162767656@qq.com>
2017-04-07 17:31:37 +0800

    添加动态数据

--- src/components/about.vue
+++ src/components/about.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>about</h1>
     <p>{{aboutMsg}}</p>
+    <h3>加载动态数据</h3>
+    <ol>
+      <li v-for="article in articles">
+        title:{{article.title}}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -10,7 +16,20 @@
     data (){
       return {
         aboutMsg:"I am about component",
+        articles:[]
       }
+    },
+    mounted:function(){
+        this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10',{},{
+            headers:{
+
+            },
+            emulateJSON:true
+        }).then(function (response) {
+            this.articles=response.data.subjects
+        },function(response){
+            console.log(response);
+        });
     }
   }
 </script>
